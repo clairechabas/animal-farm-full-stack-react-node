f5e90895dc5f2cbf8507cabdd7dffc07cdafafd9
Claire Chabas <claire.chabas@gmail.com>
2023-02-10 20:34:07 +0700

    feat(server): creating mock animals db and search endpoint

--- server/index.js
+++ server/index.js
@@ -1,19 +1,38 @@
-import express from "express";
-import cors from "cors";
-const PORT = process.env.PORT || 8081;
+import express from 'express'
+import cors from 'cors'
+const PORT = process.env.PORT || 8081
 
-const app = express();
+const app = express()
 
 // We need our frontend to be able to make requests
 // to our server from a different URL (a different origins)
-app.use(cors());
+app.use(cors())
 
 // We need to parse JSON received in requests' `body`
-app.use(express.json());
+app.use(express.json())
 
-// Build random animals
-import Chance from "chance";
-const chance = new Chance();
-const animals = [...Array(250).keys()];
+// Build an array of 250 random animals
+import Chance from 'chance'
+const chance = new Chance()
+const animals = [...Array(250).keys()].map((id) => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  }
+})
 
-app.listen(PORT, () => console.log("🚀 Listening server on port ${}"));
+// Endpoint to search within the list of animals
+app.get('/', (req, res) => {
+  const q = req.query.q?.toLowerCase() || ''
+  const results = animals.filter((animal) =>
+    animal.type.toLowerCase().includes(q)
+  )
+
+  res.send(results)
+})
+
+app.listen(PORT, () =>
+  console.log(`🚀 Listening server on http://localhost:${PORT}`)
+)
